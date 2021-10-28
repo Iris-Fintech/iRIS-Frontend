20f767ac49132e3bc01b2db118db0799a647a281
hermes926 <hermes926@gmail.com>
2021-10-28 15:28:25 +0800

    Create file

---
+++ src/components/Swap/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// // import { Link, Redirect } from 'react-router-dom';
+// import { useWeb3React } from '@web3-react/core';
+// import Web3 from 'web3';
+// import './index.css';
+
+export default function Swap(): any {
+    return 
+        (
+            <>
+                <h1>Hello World</h1>
+            </>
+        );
+}
--- src/route.tsx
+++ src/route.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './views/Home';
+import Swap from './components/Swap';
 
 const Routes = () => {
     return (
         <Switch>
             <Route exact path="/home">
                 <Home />
+            </Route>
+            <Route>
+                <Swap />
             </Route>
         </Switch>
     );
