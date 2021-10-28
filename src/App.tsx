a05362e6145e45cd91e27da4e952c6b984ade15d
AlbertLin0327 <linhsinkai@gmail.com>
2021-10-28 16:47:08 +0800

    Fix CI test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ const WalletInfo = lazy(() => import('./components/Wallet'));
 const Account = lazy(() => import('./components/Account'));
 const Routes = lazy(() => import('./route'));
 const Header = lazy(() => import('./components/Header'));
-
 import BaseConnector from './utils/baseConnection';
 
 const App = () => {
