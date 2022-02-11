3183d4e3277c400c1472e1b48b7f91e8b81e050a
AlbertLin0327 <linhsinkai@gmail.com>
2022-02-12 01:38:45 +0800

    Add responsive

--- src/components/Main/Story/index.tsx
+++ src/components/Main/Story/index.tsx
@@ -10,24 +10,22 @@ const Story = () => {
             <div className="py-3">
                 <h3 className="px-5 story-title"> The Story </h3>
 
-                <div id="story-text">
+                <div id="story-container">
                     <div id="story-paragraph-1" className="px-5">
                         <Typewriter
                             onInit={(typewriter) => {
                                 typewriter
                                     .pauseFor(2000)
                                     .changeDelay(10)
-                                    .typeString(
-                                        '<span class="story-paragraph-time">March 31, 2022...<span><br /><br />',
-                                    )
+                                    .typeString('<h5 class="story-paragraph-time">March 31, 2022...</h5><br />')
                                     .pauseFor(200)
                                     .changeDelay(1)
                                     .typeString(
-                                        '<span>&ensp;&ensp;&ensp;&ensp;</span>A piercing ringtone from NASA Johnson Space Center broke the supposed tranquility. NASA discovered a Polonium asteroid flying toward the Earth and was expected to hit the ground in 2 months. So, the scientists decided to send 1,000 per species to the new Earth – Kepler-352b. The spacecraft carried the hope of the Earth and started the journal to Kepler-352b.<br /><br />',
+                                        '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">A piercing ringtone from NASA Johnson Space Center broke the supposed tranquility. NASA discovered a Polonium asteroid flying toward the Earth and was expected to hit the ground in 2 months. So, the scientists decided to send 1,000 per species to the new Earth – Kepler-352b. The spacecraft carried the hope of the Earth and started the journal to Kepler-352b.<br /><br /></span>',
                                     )
                                     .pauseFor(200)
                                     .pasteString(
-                                        '<span>&ensp;&ensp;&ensp;&ensp;</span>However, a fragment of the Polonium asteroid hit the Hedgehog’s spacecraft and some polonium dissipated into the spacecraft. Most hedgehogs were killed but 20 of them miraculously survived and continued to fly toward Kepler-352b. Although the remaining hedgehogs were still asleep, the radiation altered their DNA and gave them mysterious power.<br /><br />',
+                                        '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">However, a fragment of the Polonium asteroid hit the Hedgehog’s spacecraft and some polonium dissipated into the spacecraft. Most hedgehogs were killed but 20 of them miraculously survived and continued to fly toward Kepler-352b. Although the remaining hedgehogs were still asleep, the radiation altered their DNA and gave them mysterious power.<br /><br /></span>',
                                         document.querySelector('#story-paragraph-1'),
                                     )
                                     .callFunction((state) => {
@@ -49,17 +47,15 @@ const Story = () => {
                                 typewriter
                                     .pauseFor(2000)
                                     .changeDelay(10)
-                                    .typeString(
-                                        '<span class="story-paragraph-time">January 19, 2038...</span><br /><br />',
-                                    )
+                                    .typeString('<h5 class="story-paragraph-time">January 19, 2038...</h5><br />')
                                     .pauseFor(200)
                                     .changeDelay(1)
                                     .typeString(
-                                        '<span>&ensp;&ensp;&ensp;&ensp;</span>Not long after the radiation asteroid crisis, the spacecraft encountered a second crisis. The solar storm from Proxima Centauri, the closest Sun to Solar System, was flooding toward the spacecraft. The interaction between solar storm substance and Polonium gave birth to a totally new element – Irisodium. The Irisodium drastically affected the hedgehogs’ DNA and they evolved into a new species – iRIS Hogs. There are 5 types of iRIS Hogs, which is Metal, Wood, Water, Fire, and Soil.<br /><br />',
+                                        '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">Not long after the radiation asteroid crisis, the spacecraft encountered a second crisis. The solar storm from Proxima Centauri, the closest Sun to Solar System, was flooding toward the spacecraft. The interaction between solar storm substance and Polonium gave birth to a totally new element – Irisodium. The Irisodium drastically affected the hedgehogs’ DNA and they evolved into a new species – iRIS Hogs. There are 5 types of iRIS Hogs, which is Metal, Wood, Water, Fire, and Soil.<br /><br /></span>',
                                     )
                                     .pauseFor(200)
                                     .pasteString(
-                                        '<span>&ensp;&ensp;&ensp;&ensp;</span>Outside of the spacecraft, the fierce solar storm kept speeding the spacecraft. The spacecraft became faster and faster, eventually, the spacecraft hit sub-light speed and headed toward Kepler-352b.<br /><br /><br />',
+                                        '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">Outside of the spacecraft, the fierce solar storm kept speeding the spacecraft. The spacecraft became faster and faster, eventually, the spacecraft hit sub-light speed and headed toward Kepler-352b.<br /><br /><br /></span>',
                                         document.querySelector('#story-paragraph-2'),
                                     )
                                     .callFunction((state) => {
@@ -81,13 +77,11 @@ const Story = () => {
                                 typewriter
                                     .pauseFor(2000)
                                     .changeDelay(10)
-                                    .typeString(
-                                        '<span class="story-paragraph-time">February 07, 2106...</span><br /><br />',
-                                    )
+                                    .typeString('<h5 class="story-paragraph-time">February 07, 2106...</h5><br />')
                                     .pauseFor(200)
                                     .changeDelay(1)
                                     .typeString(
-                                        '<span>&ensp;&ensp;&ensp;&ensp;</span>At Kepler-256b, a spacecraft landed on the planet. Meters of dust stirred up and obstructed the view. After the dust dispersed slightly, the iRIS Hogs stepped outside the spacecraft, symbolizing the new era of civilization.<br />',
+                                        '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">At Kepler-256b, a spacecraft landed on the planet. Meters of dust stirred up and obstructed the view. After the dust dispersed slightly, the iRIS Hogs stepped outside the spacecraft, symbolizing the new era of civilization.<br /></span>',
                                     )
                                     .callFunction((state) => {
                                         // turn off animation
