76102b7404a6ad03e2f014d462737786d8c89c64
AlbertLin0327 <linhsinkai@gmail.com>
2022-02-12 12:15:47 +0800

    Fix contact responsive

--- src/components/Main/Team/index.tsx
+++ src/components/Main/Team/index.tsx
@@ -13,9 +13,9 @@ const Team = () => {
         <>
             <div className="py-3">
                 <h3 className="px-5 story-title"> Our Team </h3>
-                <div className="py-4 mx-0 row">
-                    <div className="col-2"></div>
-                    <div className="col-4">
+                <div className="pt-4 mx-0 row">
+                    <div className="col-md-2"></div>
+                    <div className="col-md-4 py-2">
                         <img className="gold profile" src={gold} />
                         <a href="https://twitter.com/Albert_Lin0" target="_blank" rel="noreferrer">
                             <p className="name mt-2 mb-0">iRIS - AhkL</p>
@@ -23,7 +23,7 @@ const Team = () => {
                             <img className="twit" src={twitter} />
                         </a>
                     </div>
-                    <div className="col-4">
+                    <div className="col-md-4 py-2">
                         <img className="water profile" src={water} />
                         <a href="https://twitter.com/7ling2" target="_blank" rel="noreferrer">
                             <p className="name mt-2 mb-0">iRIS - JS</p>
@@ -31,11 +31,11 @@ const Team = () => {
                             <img className="twit" src={twitter} />
                         </a>
                     </div>
-                    <div className="col-2"></div>
+                    <div className="col-md-2"></div>
                 </div>
-                <div className="py-4 mx-0 row">
-                    <div className="col-1"></div>
-                    <div className="col-3">
+                <div className="py-0 mx-0 row">
+                    <div className="col-md-1"></div>
+                    <div className="col-md-3 py-2">
                         <img className="fire profile" src={fire} />
                         <a href="https://twitter.com/starsstarsst" target="_blank" rel="noreferrer">
                             <p className="name mt-1 mb-0">iRIS - starsstars</p>
@@ -43,7 +43,7 @@ const Team = () => {
                             <img className="twit" src={twitter} />
                         </a>
                     </div>
-                    <div className="col-4">
+                    <div className="col-md-4 py-2">
                         <img className="grass profile" src={grass} />
                         <a href="https://twitter.com/tsung_yannnn" target="_blank" rel="noreferrer">
                             <p className="name mt-1 mb-0">iRIS - tyh</p>
@@ -51,7 +51,7 @@ const Team = () => {
                             <img className="twit" src={twitter} />
                         </a>
                     </div>
-                    <div className="col-3">
+                    <div className="col-md-3 py-2">
                         <img className="soil profile" src={soil} />
                         <a href="https://twitter.com/OAO81333858" target="_blank" rel="noreferrer">
                             <p className="name mt-1 mb-0">iRIS - grass</p>
