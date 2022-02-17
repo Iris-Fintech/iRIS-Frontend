2bdf72f7c3347d753a5281a21d4f248b7d4b4f0a
AlbertLin0327 <linhsinkai@gmail.com>
2022-02-18 01:09:47 +0800

    change story

--- src/components/Main/Story/index.tsx
+++ src/components/Main/Story/index.tsx
@@ -17,21 +17,13 @@ const Story = () => {
                             <br />
                             <span>&ensp;&ensp;&ensp;&ensp;</span>
                             <span className="story-text">
-                                A piercing ringtone from NASA Johnson Space Center broke the supposed tranquility. NASA
-                                discovered a Polonium asteroid flying toward the Earth and was expected to hit the
-                                ground in 2 months. So, the scientists decided to send 1,000 per species to the new
-                                Earth – Kepler-352b. The spacecraft carried the hope of the Earth and started the
-                                journal to Kepler-352b.
-                                <br />
-                                <br />
-                            </span>
-                            <span>&ensp;&ensp;&ensp;&ensp;</span>
-                            <span className="story-text">
-                                However, a fragment of the Polonium asteroid hit the Hedgehog’s spacecraft and some
-                                polonium dissipated into the spacecraft. Most hedgehogs were killed but 20 of them
-                                miraculously survived and continued to fly toward Kepler-352b. Although the remaining
-                                hedgehogs were still asleep, the radiation altered their DNA and gave them mysterious
-                                power.
+                                Today was supposed to be a great day until a group of intelligent hedgehogs spotted a
+                                Polonium asteroid flying straight toward the Earth. After a series of calculations, the
+                                hedgehogs concluded that the asteroid would hit the ground and destroyed the planet in
+                                about two weeks. After hearing the news, the world has gone crazy, protests and wars
+                                were seen everywhere, and no one was willing to just listen. The worried hedgehogs tried
+                                to convince everyone to calm down but in vain. After the battle that raged in their
+                                head, the hedgehogs set their minds and decided to leave their beloved hometown.
                                 <br />
                                 <br />
                             </span>
@@ -46,12 +38,7 @@ const Story = () => {
                                         .pauseFor(200)
                                         .changeDelay(1)
                                         .typeString(
-                                            '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">A piercing ringtone from NASA Johnson Space Center broke the supposed tranquility. NASA discovered a Polonium asteroid flying toward the Earth and was expected to hit the ground in 2 months. So, the scientists decided to send 1,000 per species to the new Earth – Kepler-352b. The spacecraft carried the hope of the Earth and started the journal to Kepler-352b.<br /><br /></span>',
-                                        )
-                                        .pauseFor(200)
-                                        .pasteString(
-                                            '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">However, a fragment of the Polonium asteroid hit the Hedgehog’s spacecraft and some polonium dissipated into the spacecraft. Most hedgehogs were killed but 20 of them miraculously survived and continued to fly toward Kepler-352b. Although the remaining hedgehogs were still asleep, the radiation altered their DNA and gave them mysterious power.<br /><br /></span>',
-                                            document.querySelector('#story-content-1'),
+                                            '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">Today was supposed to be a great day until a group of intelligent hedgehogs spotted a Polonium asteroid flying straight toward the Earth. After a series of calculations, the hedgehogs concluded that the asteroid would hit the ground and destroyed the planet in about two weeks. After hearing the news, the world has gone crazy, protests and wars were seen everywhere, and no one was willing to just listen. The worried hedgehogs tried to convince everyone to calm down but in vain. After the battle that raged in their head, the hedgehogs set their minds and decided to leave their beloved hometown.<br /><br /></span>',
                                         )
                                         .callFunction((state) => {
                                             // turn off animation
@@ -73,20 +60,23 @@ const Story = () => {
                             <br />
                             <span>&ensp;&ensp;&ensp;&ensp;</span>
                             <span className="story-text">
-                                Not long after the radiation asteroid crisis, the spacecraft encountered a second
-                                crisis. The solar storm from Proxima Centauri, the closest Sun to Solar System, was
-                                flooding toward the spacecraft. The interaction between solar storm substance and
-                                Polonium gave birth to a totally new element – Irisodium. The Irisodium drastically
-                                affected the hedgehogs’ DNA and they evolved into a new species – iRIS Hogs. There are 5
-                                types of iRIS Hogs, which is Metal, Wood, Water, Fire, and Soil.
+                                It was the day of the apocalypse. According to history, a piercing rumble suddenly broke
+                                the supposed tranquility in the dead of night – the meteorite finally hit the earth.
+                                Brutal as it sounds, hundreds of millions of lives were taken, including the hospitable
+                                hedgehogs that were eager to save the ignorant humans. Luckily, right before the
+                                meteorite hit the ground, a group of hedgehogs had hastily grabbed their precious
+                                belongings, rushing to their spacecraft, embarking on their journey to the land of
+                                possibilities – iRIS planet.
                                 <br />
                                 <br />
                             </span>
                             <span>&ensp;&ensp;&ensp;&ensp;</span>
                             <span className="story-text">
-                                Outside of the spacecraft, the fierce solar storm kept speeding the spacecraft. The
-                                spacecraft became faster and faster, eventually, the spacecraft hit sub-light speed and
-                                headed toward Kepler-352b.
+                                However, not long after their departure, a fragment of the Polonium asteroid hit the
+                                Hedgehog’s spacecraft and some polonium penetrated the surface. Most hedgehogs suffered
+                                and were killed by the toxic substance, but six of them miraculously survived and
+                                continued to fly toward iRIS. Although the surviving hedgehogs were passed out, the
+                                radiation altered their DNA and gave them mysterious power.
                                 <br />
                                 <br />
                             </span>
@@ -102,11 +92,11 @@ const Story = () => {
                                         .pauseFor(200)
                                         .changeDelay(1)
                                         .typeString(
-                                            '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">Not long after the radiation asteroid crisis, the spacecraft encountered a second crisis. The solar storm from Proxima Centauri, the closest Sun to Solar System, was flooding toward the spacecraft. The interaction between solar storm substance and Polonium gave birth to a totally new element – Irisodium. The Irisodium drastically affected the hedgehogs’ DNA and they evolved into a new species – iRIS Hogs. There are 5 types of iRIS Hogs, which is Metal, Wood, Water, Fire, and Soil.<br /><br /></span>',
+                                            '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">It was the day of the apocalypse. According to history, a piercing rumble suddenly broke the supposed tranquility in the dead of night – the meteorite finally hit the earth. Brutal as it sounds, hundreds of millions of lives were taken, including the hospitable hedgehogs that were eager to save the ignorant humans. Luckily, right before the meteorite hit the ground, a group of hedgehogs had hastily grabbed their precious belongings, rushing to their spacecraft, embarking on their journey to the land of possibilities – iRIS planet.<br /><br /></span>',
                                         )
                                         .pauseFor(200)
                                         .pasteString(
-                                            '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">Outside of the spacecraft, the fierce solar storm kept speeding the spacecraft. The spacecraft became faster and faster, eventually, the spacecraft hit sub-light speed and headed toward Kepler-352b.<br /><br /></span>',
+                                            '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">However, not long after their departure, a fragment of the Polonium asteroid hit the Hedgehog’s spacecraft and some polonium penetrated the surface. Most hedgehogs suffered and were killed by the toxic substance, but six of them miraculously survived and continued to fly toward iRIS. Although the surviving hedgehogs were passed out, the radiation altered their DNA and gave them mysterious power.<br /><br /></span>',
                                             document.querySelector('#story-content-2'),
                                         )
                                         .callFunction((state) => {
@@ -129,10 +119,26 @@ const Story = () => {
                             <br />
                             <span>&ensp;&ensp;&ensp;&ensp;</span>
                             <span className="story-text">
-                                At Kepler-256b, a spacecraft landed on the planet. Meters of dust stirred up and
-                                obstructed the view. After the dust dispersed slightly, the iRIS Hogs stepped outside
-                                the spacecraft, symbolizing the new era of civilization.
+                                After a long journey, the hedgehogs finally arrived at iRIS. Meters of dust stirred up
+                                and obstructed the bright light outside the spacecraft. The six hedgehogs stepped out
+                                onto this foreign land and were surprised by their new superpowers. But long before they
+                                came out of their trance, the revealing beam of light caught their attention - five
+                                unknown flying subjects landed on the land of iRIS, each of them crafted with a unique
+                                symbol of Metal, Wood, Water, Fire, and Earth. As the door opened, the six hedgehogs
+                                suddenly turned to stone at the moment with more and more peculiar-looking hedgehogs
+                                running towards them. It appeared that the meteorite impact twisted the gravity and
+                                brought all alien hedgehogs together.
                                 <br />
+                                <br />
+                            </span>
+                            <span>&ensp;&ensp;&ensp;&ensp;</span>
+                            <span className="story-text">
+                                Suddenly, the dead and lonely iRIS planet was lightened up by the immersion of hedgehogs
+                                from six different planets. Loud chatters and cheerful laughters broke the deathly
+                                stillness, it was the first time where they can express their ideas without bearing the
+                                judgy faces of human beings. “Let’s build our community, a place that is filled with
+                                love, peace, and equality”, one of the hedgehogs shouted. And that’s it, a revolution in
+                                civilization is brewing and is about to blow your mind……
                                 <br />
                             </span>
                         </div>
@@ -147,7 +153,12 @@ const Story = () => {
                                         .pauseFor(200)
                                         .changeDelay(1)
                                         .typeString(
-                                            '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">At Kepler-256b, a spacecraft landed on the planet. Meters of dust stirred up and obstructed the view. After the dust dispersed slightly, the iRIS Hogs stepped outside the spacecraft, symbolizing the new era of civilization.<br /><br /></span>',
+                                            '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">After a long journey, the hedgehogs finally arrived at iRIS. Meters of dust stirred up and obstructed the bright light outside the spacecraft. The six hedgehogs stepped out onto this foreign land and were surprised by their new superpowers. But long before they came out of their trance, the revealing beam of light caught their attention - five unknown flying subjects landed on the land of iRIS, each of them crafted with a unique symbol of Metal, Wood, Water, Fire, and Earth. As the door opened, the six hedgehogs suddenly turned to stone at the moment with more and more peculiar-looking hedgehogs running towards them. It appeared that the meteorite impact twisted the gravity and brought all alien hedgehogs together.<br /><br /></span>',
+                                        )
+                                        .pauseFor(200)
+                                        .pasteString(
+                                            '<span>&ensp;&ensp;&ensp;&ensp;</span><span class="story-text">Suddenly, the dead and lonely iRIS planet was lightened up by the immersion of hedgehogs from six different planets. Loud chatters and cheerful laughters broke the deathly stillness, it was the first time where they can express their ideas without bearing the judgy faces of human beings. “Let’s build our community, a place that is filled with love, peace, and equality”, one of the hedgehogs shouted. And that’s it, a revolution in civilization is brewing and is about to blow your mind……<br /></span>',
+                                            document.querySelector('#story-content-3'),
                                         )
                                         .callFunction((state) => {
                                             // turn off animation
--- src/components/Main/Team/index.tsx
+++ src/components/Main/Team/index.tsx
@@ -6,7 +6,7 @@ import soil from '../../../assets/profile/CEP_soil.svg';
 import water from '../../../assets/profile/CEP_water.svg';
 import origin from '../../../assets/profile/CEP_origin.svg';
 import twitter from '../../../assets/twitter.svg';
-import instagram from '../../../assets/instagram.svg';
+// import instagram from '../../../assets/instagram.svg';
 
 import './index.css';
 
@@ -19,27 +19,24 @@ const Team = () => {
                     <div className="col-md-1"></div>
                     <div className="col-md-3 py-2">
                         <img className="gold profile" src={gold} />
+                        <p className="name mt-2 mb-0">iRIS - AhkL</p>
+                        <p className="pos my-0">Co-Founder | Executive</p>
                         <a href="https://twitter.com/Albert_Lin0" target="_blank" rel="noreferrer">
-                            <p className="name mt-2 mb-0">iRIS - AhkL</p>
-                            <p className="pos my-0">Co-Founder | Executive</p>
                             <img className="socialMedia" src={twitter} />
                         </a>
                     </div>
                     <div className="col-md-4 py-2">
                         <img className="water profile" src={water} />
+                        <p className="name mt-2 mb-0">iRIS - JS</p>
+                        <p className="pos my-0">Co-Founder | Finance</p>
                         <a href="https://twitter.com/7ling2" target="_blank" rel="noreferrer">
-                            <p className="name mt-2 mb-0">iRIS - JS</p>
-                            <p className="pos my-0">Co-Founder | Finance</p>
                             <img className="socialMedia" src={twitter} />
                         </a>
                     </div>
                     <div className="col-md-3 py-2">
                         <img className="origin profile" src={origin} />
-                        <a href="https://www.instagram.com/yujen.tsao/" target="_blank" rel="noreferrer">
-                            <p className="name mt-2 mb-0">iRIS - tsao</p>
-                            <p className="pos my-0">Artist | Designer</p>
-                            <img className="socialMedia" src={instagram} />
-                        </a>
+                        <p className="name mt-2 mb-0">iRIS - tsao</p>
+                        <p className="pos my-0">Artist | Designer</p>
                     </div>
                     <div className="col-1"></div>
                 </div>
@@ -47,25 +44,25 @@ const Team = () => {
                     <div className="col-md-1"></div>
                     <div className="col-md-3 py-2">
                         <img className="fire profile" src={fire} />
+                        <p className="name mt-1 mb-0">iRIS - starsstars</p>
+                        <p className="pos my-0">Co-Founder | Technology</p>
                         <a href="https://twitter.com/starsstarsst" target="_blank" rel="noreferrer">
-                            <p className="name mt-1 mb-0">iRIS - starsstars</p>
-                            <p className="pos my-0">Co-Founder | Technology</p>
                             <img className="socialMedia" src={twitter} />
                         </a>
                     </div>
                     <div className="col-md-4 py-2">
                         <img className="grass profile" src={grass} />
+                        <p className="name mt-1 mb-0">iRIS - tyh</p>
+                        <p className="pos my-0">Co-Founder | Operation</p>
                         <a href="https://twitter.com/tsung_yannnn" target="_blank" rel="noreferrer">
-                            <p className="name mt-1 mb-0">iRIS - tyh</p>
-                            <p className="pos my-0">Co-Founder | Operation</p>
                             <img className="socialMedia" src={twitter} />
                         </a>
                     </div>
                     <div className="col-md-3 py-2">
                         <img className="soil profile" src={soil} />
+                        <p className="name mt-1 mb-0">iRIS - grass</p>
+                        <p className="pos my-0">Co-Founder | Administration</p>
                         <a href="https://twitter.com/OAO81333858" target="_blank" rel="noreferrer">
-                            <p className="name mt-1 mb-0">iRIS - grass</p>
-                            <p className="pos my-0">Co-Founder | Administration</p>
                             <img className="socialMedia" src={twitter} />
                         </a>
                     </div>
