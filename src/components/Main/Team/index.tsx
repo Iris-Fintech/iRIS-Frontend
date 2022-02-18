e06c3c82291c9e389fc904f5c4924fb81e5c0632
AlbertLin0327 <linhsinkai@gmail.com>
2022-02-18 12:39:11 +0800

    add service worker

--- src/components/Main/Team/index.tsx
+++ src/components/Main/Team/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import fire from '../../../assets/profile/CEP_fire.svg';
-import gold from '../../../assets/profile/CEP_gold.svg';
-import grass from '../../../assets/profile/CEP_grass.svg';
-import soil from '../../../assets/profile/CEP_soil.svg';
-import water from '../../../assets/profile/CEP_water.svg';
-import origin from '../../../assets/profile/CEP_origin.svg';
+// import fire from '../../../assets/profile/CEP_fire.svg';
+// import gold from '../../../assets/profile/CEP_gold.svg';
+// import grass from '../../../assets/profile/CEP_grass.svg';
+// import soil from '../../../assets/profile/CEP_soil.svg';
+// import water from '../../../assets/profile/CEP_water.svg';
+// import origin from '../../../assets/profile/CEP_origin.svg';
+import outline from '../../../assets/profile/outline.svg';
 import twitter from '../../../assets/twitter.svg';
 // import instagram from '../../../assets/instagram.svg';
 
@@ -18,7 +19,7 @@ const Team = () => {
                 <div className="py-0 mx-0 row">
                     <div className="col-md-1"></div>
                     <div className="col-md-3 py-2">
-                        <img className="gold profile" src={gold} />
+                        <img className="gold profile" src={outline} />
                         <p className="name mt-2 mb-0">iRIS - AhkL</p>
                         <p className="pos my-0">Co-Founder | Executive</p>
                         <a href="https://twitter.com/Albert_Lin0" target="_blank" rel="noreferrer">
@@ -26,7 +27,7 @@ const Team = () => {
                         </a>
                     </div>
                     <div className="col-md-4 py-2">
-                        <img className="water profile" src={water} />
+                        <img className="water profile" src={outline} />
                         <p className="name mt-2 mb-0">iRIS - JS</p>
                         <p className="pos my-0">Co-Founder | Finance</p>
                         <a href="https://twitter.com/7ling2" target="_blank" rel="noreferrer">
@@ -34,7 +35,7 @@ const Team = () => {
                         </a>
                     </div>
                     <div className="col-md-3 py-2">
-                        <img className="origin profile" src={origin} />
+                        <img className="origin profile" src={outline} />
                         <p className="name mt-2 mb-0">iRIS - tsao</p>
                         <p className="pos my-0">Artist | Designer</p>
                     </div>
@@ -43,7 +44,7 @@ const Team = () => {
                 <div className="py-0 mx-0 row">
                     <div className="col-md-1"></div>
                     <div className="col-md-3 py-2">
-                        <img className="fire profile" src={fire} />
+                        <img className="fire profile" src={outline} />
                         <p className="name mt-1 mb-0">iRIS - starsstars</p>
                         <p className="pos my-0">Co-Founder | Technology</p>
                         <a href="https://twitter.com/starsstarsst" target="_blank" rel="noreferrer">
@@ -51,7 +52,7 @@ const Team = () => {
                         </a>
                     </div>
                     <div className="col-md-4 py-2">
-                        <img className="grass profile" src={grass} />
+                        <img className="grass profile" src={outline} />
                         <p className="name mt-1 mb-0">iRIS - tyh</p>
                         <p className="pos my-0">Co-Founder | Operation</p>
                         <a href="https://twitter.com/tsung_yannnn" target="_blank" rel="noreferrer">
@@ -59,7 +60,7 @@ const Team = () => {
                         </a>
                     </div>
                     <div className="col-md-3 py-2">
-                        <img className="soil profile" src={soil} />
+                        <img className="soil profile" src={outline} />
                         <p className="name mt-1 mb-0">iRIS - grass</p>
                         <p className="pos my-0">Co-Founder | Administration</p>
                         <a href="https://twitter.com/OAO81333858" target="_blank" rel="noreferrer">
